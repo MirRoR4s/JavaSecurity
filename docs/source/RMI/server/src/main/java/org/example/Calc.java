14e5b5275c3ae29974f2c34fddd7ec1ef37fd202
MirRoR4s <790356373@qq.com>
2023-09-02 08:07:51 +0800

    test

---
+++ docs/source/RMI/server/src/main/java/org/example/Calc.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+public class Calc extends UnicastRemoteObject implements ICalc {
+    public Calc() throws RemoteException {}
+    public Integer sum(List<Integer> params) throws RemoteException{
+        Integer sum = 0;
+        for (Integer param : params){
+            sum += param;
+        }
+        return sum;
+    }
+
+
+
+}
